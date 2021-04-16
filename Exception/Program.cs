89d7c66d1b0ad15342b0230a6c9e33d139c17287
TobiasCanabarro <tobiascanabarro@gmail.com>
2021-04-16 12:38:55 -0300

    Se agregro el ejercicio de excepciones

---
+++ Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+
+    public readonly string _ok_state = "Se pudo hacer la operacion!";
+    public readonly string _fail_state = "No se pudo hacer la operacion!";
+
+    public Program()
+	{
+        string state = _ok_state;
+        float result = 0;
+        int divider = 0;
+        int num = 10;
+
+		try
+        {
+            result = num / divider;
+        }
+        catch(DivideByZeroException ex)
+        {
+            value = _fail_state;
+            Console.WriteLine(ex.StackTrace);
+        }
+        finally
+        {
+            Console.WriteLine($"{value}, el resultado es {result}");
+        }
+        Console.ReadKey();
+	}
+}
