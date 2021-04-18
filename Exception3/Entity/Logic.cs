f18c7c5dc9e7b361372e3819fe84116e819da658
TobiasCanabarro <50080396+TobiasCanabarro@users.noreply.github.com>
2021-04-18 15:19:43 -0300

    Se agrego el ejercicio Exception3

---
+++ Exception3/Entity/Logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Exception3.Entity
+{
+    class Logic
+    {
+
+        public bool ConnectionBD(string strConnection)
+        {
+            bool value = true;
+            if (strConnection == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return value;
+        }
+
+    }
+}
---
+++ Exception3/Program.cs
@@ -0,0 +1,31 @@
+﻿using Exception3.Entity;
+using System;
+
+namespace Exception3
+{
+    class Program
+    {
+        private static readonly string _str_connection = "bd:192.123.239.1:2552";
+
+        static void Main(string[] args)
+        {
+
+            var logic = new Logic();
+            bool result = false;
+
+            try
+            {
+                result = logic.ConnectionBD(_str_connection);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Mensaje : {ex.Message}");
+                Console.WriteLine($"Tipo de excepcion : {ex.GetType().Name}");
+            }
+            finally
+            {
+                Console.WriteLine($"Coneccion = {result}");
+            }
+        }
+    }
+}
