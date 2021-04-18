567c83a9b54b5d21a3db10a2f125ed7ec1f2046b
TobiasCanabarro <50080396+TobiasCanabarro@users.noreply.github.com>
2021-04-18 15:08:22 -0300

    Se agrego el ejercicio Exception3

---
+++ Exception3/Entity/Logic.cs
@@ -0,0 +1,29 @@
+﻿using Exception3.Exception;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Exception3.Entity
+{
+    class Logic
+    {
+        private static readonly string _message = "Se produjo una excepcion, error al conectarse con la base de datos!";
+
+        public bool ConnectionBD(string strConnection)
+        {
+            bool value = true;
+            if (strConnection == null)
+            {
+                ThrowException();
+            }
+            return value;
+        }
+
+        private void ThrowException()
+        {
+            throw new ConnectionException(_message);
+        }
+
+    }
+}
