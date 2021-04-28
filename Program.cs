48f3e2bbe2b36373170a9ac2d6d001bf9c220fa4
TobiasCanabarro <50080396+TobiasCanabarro@users.noreply.github.com>
2021-04-28 18:35:27 -0300

    Se agrego el ejercicio ABM

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Data;
+using Logic;
+using Logic.Factory;
+using System;
+
+namespace ABM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertProd();
+            UpdateProd();
+            DeleteProd();
+        }
+
+        static void InsertProd()
+        {
+            Console.WriteLine("Insert prod!");
+            Products product = ProductFactory.Create(78, "French Fries", 2, 1, "20 bags x 4 pieces", 55, 100, 0, 0 , false);
+            ProductLogic logic = new ProductLogic();
+            logic.Add(product);
+        }
+
+        static void UpdateProd()
+        {
+            Console.WriteLine("Update prod!");
+            Products product = ProductFactory.Create(78, "French Fries", 2, 1, "20 bags x 4 pieces", 55, 0, 0, 0, true);
+            ProductLogic logic = new ProductLogic();
+            logic.Update(product);
+        }
+
+        static void DeleteProd()
+        {
+            Console.WriteLine("Delete prod!");
+            Products product = ProductFactory.Create(78, "French Fries", 2, 1, "20 bags x 4 pieces", 55, 0, 0, 0, true);
+            ProductLogic logic = new ProductLogic();
+            logic.Delete(product.ProductID);
+        }
+    }
+}
